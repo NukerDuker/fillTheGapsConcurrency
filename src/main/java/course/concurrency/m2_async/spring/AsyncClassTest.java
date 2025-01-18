6c69dbbc232b435dcad6b44a4db35dd077de0963
k.priyatnov <k.priyatnov@tbank.ru>
2025-01-18 23:16:41 +0300

    custom executor

--- src/main/java/course/concurrency/m2_async/spring/AsyncClassTest.java
+++ src/main/java/course/concurrency/m2_async/spring/AsyncClassTest.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.Executor;
